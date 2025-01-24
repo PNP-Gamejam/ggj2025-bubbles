0a0e3f27f1785e980b312d25cf0dc2338fed6058
mdechdee <dechdee.m@gmail.com>
2025-01-24 23:44:32 +0700

    implement base sprite

--- base.gd
+++ base.gd
@@ -2,8 +2,13 @@ extends Node2D
 
 @export var hp := 10
 @onready var _attackable: Attackable = $Attackable
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
 	_attackable.max_hp = hp
 	_attackable.died.connect(func(): print("game_over"))
-	_attackable.hp_changed.connect(func(): print("attacked, hp now at: %d" % _attackable.hp))
+	_attackable.hp_changed.connect(func(): 
+		print("attacked, hp now at: %d" % _attackable.hp)
+		if _attackable.hp > 0:
+			animated_sprite.frame = _attackable.max_hp / _attackable.hp
+	)
