32f62d6d40176795c789dbe79708efd62773e3e3
mdechdee <dechdee.m@gmail.com>
2025-01-25 10:37:56 +0700

    base shake on attacked

--- base.gd
+++ base.gd
@@ -9,5 +9,17 @@ func _ready() -> void:
 	_attackable.died.connect(func(): print("game_over"))
 	_attackable.hp_changed.connect(func(): 
 		if _attackable.hp > 0:
+			_shake()
 			animated_sprite.frame = _attackable.max_hp / _attackable.hp
 	)
+
+func _shake():
+	var origin = global_position
+	var tween = get_tree().create_tween()
+	tween.tween_method(func(t): 
+		global_position = origin + \
+			10 * pow(2, -.1*t) * sin(t)  * Vector2.RIGHT,
+		0, 10*PI, .5
+	)
+	tween.tween_callback(func(): global_position = origin)
+	tween.play()
